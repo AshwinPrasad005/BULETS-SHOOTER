a4b665d9bc1b653a358eb8847920417237641bc1
AshwinPrasad005 <64143870+AshwinPrasad005@users.noreply.github.com>
2020-05-08 18:55:22 +0530

    Add files via upload

--- mtOwnLibrary.js
+++ mtOwnLibrary.js
@@ -1,6 +1,10 @@
 function collision(obj1,obj2){
 
-    obj1.x-obj2.x<=obj1.width+obj2.width&&
-    obj2.x-obj2.x<=obj1.width+obj2.width;
+  if(obj1.x-obj2.x<=obj1.width+obj2.width&&
+    obj2.x-obj1.x>=obj1.width+obj2.width){
+
+       obj1.velocityX = 0; 
+
+    }
 
 }
